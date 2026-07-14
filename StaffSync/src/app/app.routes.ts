c2e17f1430fea5419b58e950d6e366c6146c5ecb
Deepjyoti Debnath <deepjyotidebnath369@gmail.com>
2026-07-14 19:00:54 +0530

    Created all the components and routings

---
+++ StaffSync/src/app/app.routes.ts
@@ -0,0 +1,31 @@
+import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Layout } from './pages/layout/layout';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Employee } from './pages/employee/employee';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: Login,
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        component: Login
+    },
+    {
+        path: 'layout',
+        component: Layout,
+        children: [
+            {
+                path: 'dashboard',
+                component: Dashboard
+            },
+            {
+                path: 'employee',
+                component: Employee
+            }
+        ]
+    }
+];
