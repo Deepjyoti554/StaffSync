e325b762bb8280c621d6cd542b02b14ac42e7c99
Deepjyoti Debnath <deepjyotidebnath369@gmail.com>
2026-07-20 12:13:26 +0530

    Added login page and logic to authenticate user

--- StaffSync/src/app/pages/login/login.ts
+++ StaffSync/src/app/pages/login/login.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login { }
+export class Login {
+  loginUser: any = {
+    username: '',
+    password: ''
+  }
+
+  url: any = "https://6a5bf4d864f700df5bd7a2a5.mockapi.io/LoginUser";
+
+  //Creating the http client to send the login request to the server
+  http = inject(HttpClient);
+  route = inject(Router);
+
+  onLogin() {
+    debugger;
+    console.log("Login button clicked");
+    const loginData = {
+      message: "Login Successful",
+      result: true,
+      data: [this.loginUser],
+      id: ""
+    }
+    this.http.post(this.url, loginData).subscribe((res: any) => {
+      localStorage.setItem('userData', JSON.stringify(this.loginUser));
+      alert("Login Successful");
+      this.route.navigate(['/layout/dashboard']);
+    })
+  }
+}
